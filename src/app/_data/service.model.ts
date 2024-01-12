0b0b6e0c64ca7144335bd858000e056e886ae7aa
Pawel Rygiewicz <p.rygiewicz@gmail.com>
2024-01-12 13:20:49 +0100

    Introduce RequestState

--- src/app/_data/service.model.ts
+++ src/app/_data/service.model.ts
@@ -1,3 +1,35 @@
 export type FilterParams = Record<string, string>;
 
 export const EMPTY_FILTERS: FilterParams = {};
+
+export interface RequestState<T> {
+  error?: string;
+  data?: T;
+  loading: boolean;
+}
+
+export function RS_EMPTY(): RequestState<any> {
+  return {
+    loading: false,
+  };
+}
+
+export function RS_LOADING(): RequestState<any> {
+  return {
+    loading: true,
+  };
+}
+
+export function RS_ERROR(error: string): RequestState<any> {
+  return {
+    error,
+    loading: false,
+  };
+}
+
+export function RS_SUCCESS<T>(data: T): RequestState<T> {
+  return {
+    data,
+    loading: false,
+  };
+}
