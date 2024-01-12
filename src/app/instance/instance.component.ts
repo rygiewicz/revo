3c6b3f5cee8c8cea0c5795675ad87fcc1221e4bf
Pawel Rygiewicz <p.rygiewicz@gmail.com>
2024-01-12 12:53:29 +0100

    Add instance-table

--- src/app/instance/instance.component.ts
+++ src/app/instance/instance.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-instance',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './instance.component.html',
-  styleUrl: './instance.component.scss'
+  styleUrl: './instance.component.scss',
 })
 export class InstanceComponent {
-
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 }
