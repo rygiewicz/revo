8e908861b684a0c9aacfbbff1f5e3430e9bcc01f
Pawel Rygiewicz <p.rygiewicz@gmail.com>
2024-01-12 12:53:09 +0100

    Add link to /instances

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, MatButtonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
